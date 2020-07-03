ccce71bad551e4548e3a2eb520f66a00c21cc25e
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-03 19:54:02 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,43 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  
+  counter = 0
+  
+  while counter < coupons.length 
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)    
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else
+        cart_item_with_coupon = {
+          :item => couponed_item_name, 
+          :price => coupons[counter][:cost] / coupons[counter][:num], 
+          :clearance => cart_item[:clearance], 
+          :count => coupons[counter][:num]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
+      end
+    end
+    counter += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < cart.length
+    if cart[counter][:clearance] == TRUE
+      cart[counter][:price] = cart[counter][:price]*0.8.round(2)
+    end
+    counter += 1
+  end
+  cart
 end
 
 def checkout(cart, coupons)
@@ -22,4 +50,25 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  
+  new_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(new_cart, coupons)
+  clearanced_and_couponed_cart = apply_clearance(couponed_cart)
+  
+  
+  counter = 0
+  total = 0
+  final_total = 0 
+  
+  while counter < clearanced_and_couponed_cart.length
+    total += clearanced_and_couponed_cart[counter][:price]*clearanced_and_couponed_cart[counter][:count]
+    counter += 1
+  end
+  
+  if total > 100
+    final_total = total*0.9.round(2)
+  else
+    final_total = total.round(2)
+  end
+  final_total
 end
